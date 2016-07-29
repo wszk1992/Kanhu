bf8d2449433b2072564c34d45e4acd69084aec55
Kan Zheng <wszk1992@gmail.com>
2016-07-29 21:41:22 +0800

    update login and register

--- router/index.js
+++ router/index.js
@@ -1,11 +1,14 @@
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-		res.render('index.html');
+		res.render('index.html', {title: 'Kanhu'});
 	});
 	app.get('/login', function(req, res) {
 		res.render('login.html');
 	});
 	app.get('/about', function(req, res) {
 		res.render('about.html');
+	});
+	app.get('/register', function(req, res) {
+		res. render('register.html', {title: 'Register in Kanhu'});
 	});
 }
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var app = express();
+
 require('./router/index')(app);
+
+//view engine setup
+//allow nodejs use html
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
 var server = app.listen(3000, function() {
 	console.log("Express on port 3000!");
 });
